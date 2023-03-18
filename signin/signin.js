09ac13acb371e69534e30aff8b5837a829fdcf19
pankaj511995 <kpankaj511995@gmail.com>
2023-03-18 22:42:51 +0530

    chat app frontend

--- signin/signin.js
+++ signin/signin.js
@@ -3,18 +3,17 @@
 document.getElementById('signinuser').addEventListener('click',async(e)=>{
     try{
     e.preventDefault()
-    const obj={
-        email:document.getElementById('email').value,
-        password:document.getElementById('password').value
-    }
-    if(obj.email===''||obj.password===''){
-        return message('Oh! please fill all field')
-    }
-   const result= await axios.post('http://localhost:3000/user/signin',obj)
-   localStorage.setItem('username_group_chat',result.data.token)
-    message(result.data.message)
+            const obj={
+                email:document.getElementById('email').value,
+                password:document.getElementById('password').value
+            }
+        if(obj.email===''||obj.password===''){
+            return message('Oh! please fill all field')
+        }
+    const result= await axios.post('http://localhost:3000/user/signin',obj)
+    localStorage.setItem('username_group_chat',result.data.token)
+        message(result.data.message)
 }catch(err){
-    
     message(err.response.data.message)
 }
 
