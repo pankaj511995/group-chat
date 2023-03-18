46c33a000f11e29be5d9630ab82b79efb1edd4bf
pankaj511995 <kpankaj511995@gmail.com>
2023-03-18 20:13:10 +0530

    signup success

---
+++ signup.js
@@ -0,0 +1,25 @@
+
+
+document.getElementById('submitform').addEventListener('click',async(e)=>{
+    try{
+    e.preventDefault()
+    const obj={
+        name:document.getElementById('name').value,
+        email:document.getElementById('email').value,
+        phone:document.getElementById('phone').value,
+        password:document.getElementById('password').value
+    }
+    // if(obj.name===''||obj.email===''||obj.phone===''||obj.password===''){
+    //     return error('Oh! please fill all field')
+    // }
+   const result= await axios.post('http://localhost:3000/user/signup',obj)
+    message(result.data.message)
+}catch(err){
+    
+    message(err.response.data.message)
+}
+
+})
+function message(message){
+    document.getElementById('error').innerHTML=message
+}
