040140cf159c5212d78a9467ef74150612f3be12
pankaj511995 <kpankaj511995@gmail.com>
2023-03-18 21:16:04 +0530

    signin page

---
+++ signin/signin.js
@@ -0,0 +1,23 @@
+
+
+document.getElementById('signinuser').addEventListener('click',async(e)=>{
+    // try{
+    e.preventDefault()
+    const obj={
+        email:document.getElementById('email').value,
+        password:document.getElementById('password').value
+    }
+    // if(obj.email===''||obj.password===''){
+    //     return message('Oh! please fill all field')
+    // }
+   const result= await axios.post('http://localhost:3000/user/signin',obj)
+    message(result.data.message)
+// }catch(err){
+    
+//     message(err.response.data.message)
+// }
+
+})
+function message(message){
+    document.getElementById('error').innerHTML=message
+}
--- signup/signup.js
+++ signup/signup.js
@@ -9,9 +9,9 @@ document.getElementById('submitform').addEventListener('click',async(e)=>{
         phone:document.getElementById('phone').value,
         password:document.getElementById('password').value
     }
-    // if(obj.name===''||obj.email===''||obj.phone===''||obj.password===''){
-    //     return error('Oh! please fill all field')
-    // }
+    if(obj.name===''||obj.email===''||obj.phone===''||obj.password===''){
+        return message('Oh! please fill all field')
+    }
    const result= await axios.post('http://localhost:3000/user/signup',obj)
     message(result.data.message)
 }catch(err){
