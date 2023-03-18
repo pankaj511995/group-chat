ba3d8096881ecf95667bdd758c9b3ea67995c4dc
pankaj511995 <kpankaj511995@gmail.com>
2023-03-18 21:53:40 +0530

    sign up backend

--- signin/signin.js
+++ signin/signin.js
@@ -1,21 +1,22 @@
 
 
 document.getElementById('signinuser').addEventListener('click',async(e)=>{
-    // try{
+    try{
     e.preventDefault()
     const obj={
         email:document.getElementById('email').value,
         password:document.getElementById('password').value
     }
-    // if(obj.email===''||obj.password===''){
-    //     return message('Oh! please fill all field')
-    // }
+    if(obj.email===''||obj.password===''){
+        return message('Oh! please fill all field')
+    }
    const result= await axios.post('http://localhost:3000/user/signin',obj)
+   localStorage.setItem('username_group_chat',result.data.token)
     message(result.data.message)
-// }catch(err){
+}catch(err){
     
-//     message(err.response.data.message)
-// }
+    message(err.response.data.message)
+}
 
 })
 function message(message){
