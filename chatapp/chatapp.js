304cd8cdd70ba92049e79c4d476714402fe99431
pankaj511995 <kpankaj511995@gmail.com>
2023-03-19 21:35:02 +0530

    api call only for last message rest stored in local storage

--- chatapp/chatapp.js
+++ chatapp/chatapp.js
@@ -1,6 +1,6 @@
 const token=localStorage.getItem('username_group_chat')
 const username=parseJwt(token)
-
+const perpage=5
 const allchat=document.getElementById('listOfAllChat')
 
 function addInChatBox(message,name,myself){
@@ -29,6 +29,17 @@ document.getElementById('sendessage').addEventListener('click',async(e)=>{
     const message=document.getElementById('inputfield').value
     const result= await axios.post('http://localhost:3000/message/send',{message:message},{headers:{'token':token}})
     addInChatBox(message,username.name,true)
+    const storageitem=localStorage.getItem('user_chat_all')
+    let store=[]
+    if(storageitem){
+        store=JSON.parse(storageitem)
+        if(store.length>perpage){
+            const length=store.length
+            store=store.slice(length-perpage)
+        }
+    }
+    store.push(result.data)
+    localStorage.setItem('user_chat_all',JSON.stringify(store))
     document.getElementById('inputfield').value=''
 }catch(err){
     
@@ -36,27 +47,51 @@ document.getElementById('sendessage').addEventListener('click',async(e)=>{
 }
 })
 document.addEventListener('DOMContentLoaded',async()=>{
-    const result= await axios.get('http://localhost:3000/message/allmessage',{headers:{'token':token}})
-    console.log(result.data)
-    result.data.forEach(e=>{
-        if(username.id===e.UserId){
-            addInChatBox(e.message,e.name,true)
-        }else{
-            addInChatBox(e.message,e.name,false)
-        }
-    })
+    const result=localStorage.getItem('user_chat_all')
+    if(result){
+        localstoragecall(JSON.parse(result))
+    }
 })
-setInterval(async()=>{
+function localstoragecall(arr){
     allchat.innerHTML=''
-    const result= await axios.get('http://localhost:3000/message/allmessage',{headers:{'token':token}})
-    result.data.forEach(e=>{
+    arr.forEach(e=>{
         if(username.id===e.UserId){
             addInChatBox(e.message,e.name,true)
         }else{
             addInChatBox(e.message,e.name,false)
         }
     })
-},2000)
+}
+setInterval(async()=>{
+    try{
+        const token=localStorage.getItem('username_group_chat')
+    const result1=localStorage.getItem('user_chat_all')
+    if(result1){
+        const result=JSON.parse(result1)
+    const lastmessage=result[result.length-1].id
+     const newmessage= await axios.get(`http://localhost:3000/message/newmessage?lastmessage=${lastmessage}`,{headers:{'token':token}})
+     if(newmessage.data.length>0){
+        newmessage.data.forEach(e=>{
+             addInChatBox(e.message,e.name,false)
+        })
+  
+    let store=[]
+    store=[...result,...newmessage.data]
+        if(store.length>perpage){
+            const length=store.length
+            store=store.slice(length-perpage)
+        }
+    localStorage.setItem('user_chat_all',JSON.stringify(store))
+    }
+   
+    }
+}catch(err){
+    console.log('sorry timer failed')
+}
+},5000)
+
+
+
 function parseJwt (token) {
     var base64Url = token.split('.')[1];
     var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
